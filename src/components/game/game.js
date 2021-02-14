ead755ee6dfad90de12ccef0df47d12291b294ec
anil verma <anilcs240@gmail.com>
2021-02-14 16:41:23 +0530

    added car and stack

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import Matter from 'matter-js'
 
-export default class Game extends Component {
-    gameDv = React.createRef();
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Composites = Matter.Composites,
+    Composite = Matter.Composite,
+    Mouse = Matter.Mouse,
+    Constraint = Matter.Constraint,
+    Body = Matter.Body;
+
+var engine = Engine.create({
+
+});
+export default class SimpleObs extends Component {
+    methodDv = React.createRef();
+    car;
     componentDidMount() {
 
-        var Engine = Matter.Engine,
-            Render = Matter.Render,
-            World = Matter.World,
-            Bodies = Matter.Bodies
-
-        var engine = Engine.create({
-
-        });
         //create renderer on screen
         var render = Render.create({
-            element: this.gameDv.current,
+            element: this.methodDv.current,
             engine: engine,
             options: {
                 width: 800,
@@ -24,23 +30,37 @@ export default class Game extends Component {
             }
         });
 
-        var ballC = Bodies.rectangle(260, 60, 80, 70, {
-            //isStatic: true,
-            restitution: 1,
-            render: { fillStyle: 'blue' }
-        })
-        var ground = Bodies.rectangle(400, 380, 800, 70, { isStatic: true });
-
-        World.add(engine.world, [ballC, ground]);
+        this.car = Composites.car(190, 100, 100, 45, 30);
+        var boxes = Composites.stack(500, 80, 3, 1, 10, 0, function (x, y) {
+            return Bodies.rectangle(x, y, 50, 40);
+        });
+        var chain = Composites.chain(boxes, 0.5, 0, -0.5, 0, { stiffness: 1 });
+        Composite.add(boxes, Constraint.create({
+            bodyA: boxes.bodies[0],
+            pointB: { x: 500, y: 15 },
+            stiffness: 0.8
+        }));
+        World.add(engine.world, [this.car, boxes, Bodies.rectangle(400, 0, 810, 30, { isStatic: true }),
+        Bodies.rectangle(400, 400, 810, 30, { isStatic: true }),
+        Bodies.rectangle(800, 200, 30, 420, { isStatic: true }),
+        Bodies.rectangle(0, 200, 30, 420, { isStatic: true })]);
 
         Engine.run(engine);
         Render.run(render);
     }
+
+    moveForward = () => {
+        Body.applyForce(this.car.bodies[0], { x: this.car.bodies[0].position.x, y: this.car.bodies[0].position.y }, { x: 0.5, y: 0 });
+    }
+
+
     render() {
         return (
-            <div ref={this.gameDv} >
+            <div>
+                <button onClick={this.moveForward}>Move Forward</button>
+                <div ref={this.methodDv} >
 
-            </div>
+                </div></div>
         )
     }
 }
